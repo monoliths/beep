f114edb2ed89f5d36bc763361898b1792149c540
gerald hernandez <gerald2557@gmail.com>
2017-06-22 17:41:06 -0700

    added firebase authentication

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular'
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular'
+import { AngularFireAuth } from 'angularfire2/auth'
+import { Account } from '../../models/account/account.interface';
+import { LoginResponse } from '../../models/login/login-response.interface';
 
 /**
  * Generated class for the LoginFormComponent component.
@@ -13,13 +16,31 @@ import { NavController } from 'ionic-angular'
 })
 export class LoginFormComponent {
 
-  text: string;
+  account = {} as Account;
 
-  constructor(private navCtrl: NavController) {
+  @Output() loginStatus: EventEmitter<LoginResponse>;
+
+  constructor(private navCtrl: NavController, private afAuth: AngularFireAuth, private toast:ToastController) {
+    this.loginStatus = new EventEmitter<LoginResponse>();
   }
 
-  navigateToPage(pageName:string): void {
-    pageName === 'TabsPage' ? this.navCtrl.setRoot(pageName) : this.navCtrl.push(pageName);
+  async login() {
+    try {
+      const result: LoginResponse = { 
+        result: await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password)
+      }
+      this.loginStatus.emit(result);
+    } catch (e) {
+        console.error(e);
+        const error: LoginResponse = {
+          error: e
+        }
+        this.loginStatus.emit(error);
+    }
+  }
+
+  navigateToRegisterPage() {
+    this.navCtrl.push('RegisterPage');
   }
 
 }
